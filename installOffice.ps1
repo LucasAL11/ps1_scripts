795d62a7fd7cdab0992ddd6c5bb5165e3f2956c5
lucas luna <lucasluna1000@gmail.com>
2024-10-18 21:39:40 -0300

    teste

--- installOffice.ps1
+++ installOffice.ps1
@@ -1,4 +1,5 @@
 # Define nova URL para baixar a Ferramenta de Implantação do Office
+#@Todo verificar se link e valido
 $officeDeploymentToolUrl = "https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_17830-20162.exe"
 $downloadPath = "$env:TEMP\officedeploymenttool.exe"
 $configXmlPath = "$env:TEMP\configuration.xml"
